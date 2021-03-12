dca33eb9d41b2e6c316c47fbb18619f8ecc2ac7e
João V. Louzada <joao.louzada@pixter.com.br>
2021-03-11 21:35:40 -0300

    feat: initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./assets/logo.png";
+
+import OverviewIcon from "./Icons/OverviewIcon.svg";
+import LogoutIcon from "./Icons/LogoutIcon.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="Sidebar">
+        <div className="LogoDiv">
+          <img src={Logo} alt="Logo da imagem da empresa" />
+          <hr />
+        </div>
+        <div className="OptionsDiv">
+          <a onClick={() => console.log("Clicou")} className="OptionsButton">
+            <img src={OverviewIcon} />
+            <span>Overview</span>
+          </a>
+          <a onClick={() => console.log("Clicou")} className="OptionsButton">
+            <img src={OverviewIcon} />
+            <span>Cloud</span>
+          </a>
+          <a onClick={() => console.log("Clicou")} className="OptionsButton">
+            <img src={OverviewIcon} />
+            <span>Sketch</span>
+          </a>
+          <a onClick={() => console.log("Clicou")} className="OptionsButton">
+            <img src={OverviewIcon} />
+            <span>Experiments</span>
+          </a>
+          <a onClick={() => console.log("Clicou")} className="OptionsButton">
+            <img src={OverviewIcon} />
+            <span>Security</span>
+          </a>
+          <a onClick={() => console.log("Clicou")} className="OptionsButton">
+            <img src={OverviewIcon} />
+            <span>Ownership</span>
+          </a>
+          <a onClick={() => console.log("Clicou")} className="OptionsButton">
+            <img src={OverviewIcon} />
+            <span>A/B Test</span>
+          </a>
+          <a onClick={() => console.log("Clicou")} className="OptionsButton">
+            <img src={OverviewIcon} />
+            <span>Colors</span>
+          </a>
+        </div>
+        <div className="LogOutDiv">
+          <a
+            onClick={() => alert("Você foi deslogado com sucesso.")}
+            className="OptionsButton"
+          >
+            <img src={LogoutIcon} />
+            <span>Logout</span>
+          </a>
+        </div>
+      </section>
+      <section className="Content">
+        <h3>Logout</h3>
+      </section>
     </div>
   );
 }
